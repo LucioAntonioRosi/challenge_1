36bccb57ea01d7823977d40a289ddeba3e320357
LucioAntonioRosi <lucioantoniorosi@gmail.com>
2024-03-10 11:12:25 +0000

    Modified main and .json (muparser still not good)

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CXXFLAGS = -std=c++20
-CPPFLAGS = -I ./include -I/usr/include -I/usr/include/x86_64-linux-gnu/ # I am not able to include <nlohmann/json>
+CPPFLAGS = -I ./include -I/usr/include -I/usr/include/x86_64-linux-gnu/ # I do not know why but the directory bits is in the last one
 LDFLAGS = -L. -Wl,-rpath=${PWD}
 LIBS = -lmuparserx
 
-.PHONY: all clean distclean
-
+.phony= all clean distclean
+.DEFAULT_GOAL = all
 all: main
 
 main: main.o
--- main.cpp
+++ main.cpp
@@ -3,74 +3,96 @@
 #include <tuple>
 #include <fstream>
 #include <vector>
-#include "nlohmann/json.hpp"
+#include <nlohmann/json.hpp>
 #include "mpParser.h"
+//#include "minimum.h"
 
 using namespace mup;
 
-// #include <vector>
-// #include <cmath>
-
-// Function to compute the minimum of a function
-
-double computeMinimum()
-{
-   // Create a parser
-ParserX parser(pckALL_NON_COMPLEX);
-std::cout << "Parser created" << std::endl;
-// Define the values
-Value xVal(1.1), yVal(2.0);
-
-// Define the variables
-
-Variable x(&xVal), y(&yVal) ; 
-
- parser.DefineVar("x", x);
- parser.DefineVar("y", y);
-
-// Get the function from the user
-
-std::string function;
-std::cout << "Enter a function in x and y: ";
-std::getline(std::cin, function);
-
-// Set the function
-parser.SetExpr(function);
-
- // Placeholder return value
-    Value result = parser.Eval();
-    std::cout << "Result: " << result << std::endl;
-    return 0;
-}
-
 int main()
 {
-    // Call the computeMinimum function
-    double minimum = computeMinimum();
-
-    // Print the minimum value
-    std::cout << "Minimum value: " << minimum << std::endl;
-
-     std::ifstream file("parameters.json");
+    std::ifstream file("parameters.json");
 
     // Parse the JSON file
+    
     nlohmann::json j;
     file >> j;
-
-    // Get the parameters
-    double a0 = j["a0"];
-    std::string decay_string1 = j["decay"]["string1"];
-    std::string decay_string2 = j["decay"]["string2"];
-    std::string decay_string3 = j["decay"]["string3"];
-    double sigma = j["sigma"];
-    int iter = j["iter"];
+    
+    // Get the parameters from the JSON file
+    
+    const std::tuple <double, int, std::string, std::string, std::string, std::string, double, int> &parameters = std::make_tuple(
+    j["a0"],j["dim"], j["decay"]["string1"], j["decay"]["string2"], j["decay"]["string3"],j["gradient"], j["sigma"], j["iter"]);
 
     // Print the parameters
-    std::cout << "a0: " << a0 << std::endl;
-    std::cout << "decay_string1: " << decay_string1 << std::endl;
-    std::cout << "decay_string2: " << decay_string2 << std::endl;
-    std::cout << "decay_string3: " << decay_string3 << std::endl;
-    std::cout << "sigma: " << sigma << std::endl;
-    std::cout << "iter: " << iter << std::endl;
-    return 0;
+    std::cout << "Parameters: " << std::get<0>(parameters) << ", " << std::get<1>(parameters) << ", " << std::get<2>(parameters) << ", " << std::get<3>(parameters) << ", " << std::get<4>(parameters) << ", " << std::get<5>(parameters) << std::endl;
+    
+    // Create a parser
+    ParserX parser(pckALL_NON_COMPLEX);
+    std::vector<ParserX> parserVec;
+    parserVec.reserve(std::get<1>(parameters));
+    std::cout << "Parser created and Parser vec created" << std::endl;
+    
+    // Define the values and the variables
+    std::vector<mup::Value> values;
+    std::vector<mup::Variable> variables;
+    
+    //mup::Value value(1.0); // Initialize the Value with 1.0
+    //mup::Variable variable(&value);
+
+    // Define the variables in parser
+    
+    for (int i = 0; i < 2; ++i) {
+
+    mup::Value* value = new mup::Value(i + 1.0); // Initialize the Value with i + 1.0
+    mup::Variable variable(value);
+    // Add the Value and Variable to the vectors
+    values.push_back(&value);
+    variables.push_back(&variable);
+
+    // Define the variable in the parser with a dynamically generated name
+    std::string varName = "x" + std::to_string(i);
+    parser.DefineVar(varName, variables[i]);
+    std::cout << "Variable defined: " <<varName << std::endl;
 }
+
+    for (int i = 0; i < std::get<1>(parameters); ++i) {
+        parserVec.push_back(parser);
+    }
+
+    std::cout << "Parser vec size: " << parserVec.size() << std::endl;
+    
+    value = 2.0;
+    
+    std::string function;
+    std::cout << "Enter a function in " << 2 << "  variables (x0, x1, x2, ...): ";
+    std::getline(std::cin, function);
+    parser.SetExpr(function);
+
+    Value result = parser.Eval();
+    
+    std::cout << "Result: " << result << std::endl;
+
+    // Define the gradient
+
+    std::cout << "Do you want to define the gradient by yourself? (y/n): "<< std::endl;
+        for (std::size_t i = 0; i < 2; ++i)
+        {
+            std::string function;
+            std::cout << "Enter a function in " << 2 << "  variables (x0, x1, x2, ...): ";
+
+            std::getline(std::cin, function);
+            try {
+            parserVec[i].SetExpr(function);
+        } catch (mup::ParserError& e) {
+            std::cout << "Error: " << e.GetMsg() << std::endl;
+        }
+        }
+
+        
+    return 0;
+    }
+
+
+
+
+
